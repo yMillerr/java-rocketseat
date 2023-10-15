fd963392df61066be393ccdd017a6b3502034bf3
ymillerr <millerandvitor@gmail.com>
2023-10-15 15:05:39 +0200

    first commit

---
+++ src/main/java/br/com/rocketseat/todoList/errors/ExceptionHandleController.java
@@ -0,0 +1,16 @@
+package br.com.rocketseat.todoList.errors;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
+public class ExceptionHandleController {
+
+  @ExceptionHandler(HttpMessageNotReadableException.class)
+  public ResponseEntity<String> handleHttpMessageNotReadableException(HttpMessageNotReadableException e) {
+    return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getMostSpecificCause().getMessage());
+  }
+}
